6ad218cb009f9eb662675fe3fe7586bcd6c17a88
Kimmo Koskinen <kimmo@tiuhti.net>
2023-03-01 22:55:53 +0200

    Use crac to push compilation to checkpoint time

--- src/layer_demo/core.clj
+++ src/layer_demo/core.clj
@@ -2,4 +2,4 @@
   (:require [clj-http.client :as http]))
 
 (defn get-clojure []
-  (-> (http/get "https://clojure.org") :body (subs 0 20)))
+  (-> (http/get "https://clojure.org") :body (subs 0 40)))
--- src/layer_demo/handler.clj
+++ src/layer_demo/handler.clj
@@ -2,9 +2,25 @@
 
 (gen-class
   :name "layer_demo.handler"
-  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler
+               org.crac.Resource]
+  :post-init register-crac)
 
 (defn -handleRequest [this in out ctx]
+  (println "Running handler")
   (let [get-clojure (requiring-resolve 'layer-demo.core/get-clojure)]
     (println (get-clojure))
     (spit out (get-clojure))))
+
+;; crac stuff
+
+(defn -register-crac [this]
+  (.register (org.crac.Core/getGlobalContext) this))
+
+(defn -beforeCheckpoint [this context]
+  (println "Before checkpoint")
+  ((requiring-resolve 'layer-demo.core/get-clojure))
+  (println "Before checkpoint done"))
+
+(defn -afterRestore [this context]
+  (println "After restore"))
